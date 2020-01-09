6e7c4831356dd1eb669e3f4ad443b637452c9356
DrugKing <59591853+DrugKing@users.noreply.github.com>
2020-01-09 11:37:03 +0800

    Pistol
    The script for shooting and some photon id fix

---
+++ Assets/Script/SimpleShoot.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleShoot : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 50f;
+    public GameObject bulletPrefab;
+    public GameObject casingPrefab;
+    public GameObject muzzleFlashPrefab;
+    public Transform barrelLocation;
+    public Transform casingExitLocation;
+    public Camera fpsCamera;
+
+    public float shotPower = 100f;
+
+    void Start()
+    {
+        if (barrelLocation == null)
+            barrelLocation = transform;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GetComponent<Animator>().SetTrigger("Fire");
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet;
+        bullet = Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
+
+        GameObject tempFlash;
+        //Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation).GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
+        tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
+
+        Destroy(tempFlash, 2.0f);
+        Destroy(bullet, 0.5f);
+        //  Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation).GetComponent<Rigidbody>().AddForce(casingExitLocation.right * 100f);
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            target Target = hit.transform.GetComponent<target>();
+            if(Target!= null)
+            {
+                Target.damaged(damage);
+            }
+        }
+    }
+
+    void CasingRelease()
+    {
+         GameObject casing;
+        casing = Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation) as GameObject;
+        casing.GetComponent<Rigidbody>().AddExplosionForce(550f, (casingExitLocation.position - casingExitLocation.right * 0.3f - casingExitLocation.up * 0.6f), 1f);
+        casing.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(10f, 1000f)), ForceMode.Impulse);
+        Destroy(casing, 0.5f);
+    }
+
+
+}
